25e68da07e2a07733c638717fb3507a7f3c3e9a5
d3rpp <github@d3rpp.dev>
2024-05-06 17:08:34 +1200

    added extra comments in test macro invocation

--- crates/types/src/types/test/mod.rs
+++ crates/types/src/types/test/mod.rs
@@ -1,1 +1,4 @@
+// rust-analyzer says - path = "", path_parent = None
+// rustc says         - path = "crates/types/src/types/test/mod.rs", path_parent = Some("crates/types/src/types/test")
+
 gen_test_eg!();
